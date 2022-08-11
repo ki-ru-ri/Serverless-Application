4f3e051a67fad597687d1789e33d118c3695f1aa
ki-ru-ri <rkm.mwangi@gmail.com>
2022-08-11 21:45:09 +0300

    updated config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 const apiId = 'igv36jiigj';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
    // TODO: Create an Auth0 application and copy values from it into this map. For example:
